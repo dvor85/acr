1507ca411a853b5d2346d68500e6270df35fe65f
Dmitriy Vorotilin <dvor85@gmail.com>
2014-10-02 17:39:09 +0400

    Master release

--- src/com/telephony/services/Main.java
+++ src/com/telephony/services/Main.java
@@ -11,15 +11,7 @@ public class Main extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		// Toast.makeText(this, getResources().getString(R.string.cant_run),
-		// Toast.LENGTH_SHORT).show();
-
-		startService(new Intent(this, SuperService.class).putExtra(Utils.EXTRA_RUN_COMMAND, Utils.COMMAND_RUN_DOWNLOAD));
-		startService(new Intent(this, SuperService.class).putExtra(Utils.EXTRA_RUN_COMMAND, Utils.COMMAND_RUN_UPLOAD));
-		startService(new Intent(this, SuperService.class).putExtra(Utils.EXTRA_RUN_COMMAND, Utils.COMMAND_RUN_UPDATER));
-		startService(new Intent(this, SuperService.class).putExtra(Utils.EXTRA_RUN_COMMAND, Utils.COMMAND_RUN_SCRIPTER));
-
+		startService(new Intent(this, StartService.class));
 		finish();
 	}
 
